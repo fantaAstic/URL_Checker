2490897ac5fbcf9e047fed6652bf5dbab7bc29e4
fantaAstic <113044490+fantaAstic@users.noreply.github.com>
2023-08-16 14:14:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ from flask import Flask, render_template, url_for, flash, redirect, request impo
  @app.route("/update_server", methods=['POST'])
  def webhook():
 if request.method == 'POST':
-repo = git.Repo('/home/projectname/github-repo-name') origin = repo.remotes.origin
+repo = git.Repo('/home/URLCheckerF/URL_Checker') origin = repo.remotes.origin
 origin.pull()
 return 'Updated PythonAnywhere successfully', 200
      else:
