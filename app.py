ce78db087282acc8f5704ac71ad92540aa3fe1c1
fantaAstic <113044490+fantaAstic@users.noreply.github.com>
2023-08-16 14:13:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -171,6 +171,17 @@ def display_username(form):
         return random_name
     return form.username.data  # Return actual username if consent was given
 
+# for python anywhere deployment
+from flask import Flask, render_template, url_for, flash, redirect, request import git
+
+ @app.route("/update_server", methods=['POST'])
+ def webhook():
+if request.method == 'POST':
+repo = git.Repo('/home/projectname/github-repo-name') origin = repo.remotes.origin
+origin.pull()
+return 'Updated PythonAnywhere successfully', 200
+     else:
+         return 'Wrong event type', 400
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
