916defb42f849267a4d5208b2a6a3215d40ed9d9
fantaAstic <113044490+fantaAstic@users.noreply.github.com>
2023-08-16 14:34:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ def webhook():
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
      else:
-         return 'Wrong event type', 400
+        return 'Wrong event type', 400
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
