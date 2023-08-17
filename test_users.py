71c012f524ff36dd4cc3df209dfb7441d630b5dd
fantaAstic <113044490+fantaAstic@users.noreply.github.com>
2023-08-17 11:27:57 +0100

    Update test_users.py

--- test_users.py
+++ test_users.py
@@ -1,8 +1,9 @@
-import unittest, sys, os
-from flask_sqlalchemy import SQLAlchemy
+# this file contains tests on users, login and registration
+
+import unittest, sys
 
 sys.path.append('../webdev') # imports python file from parent directory
-from app import app, db
+from app import app
 
 class UsersTests(unittest.TestCase):
 
@@ -10,10 +11,6 @@ class UsersTests(unittest.TestCase):
     def setUp(self):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site_testing.db'
         self.app = app.test_client()
-
-    ###############
-    #### tests ####
-    ###############
 
     def register(self, username, email, password):
         return self.app.post('/register',
@@ -28,54 +25,6 @@ class UsersTests(unittest.TestCase):
         response = self.register('test', 'test@example.com', 'FlaskIsAwesome')
         self.assertEqual(response.status_code, 200)
 
-# need to add following logic in html response before actually being able to test it.
-    '''
-    def test_existing_user_registration(self):
-        # Register the first user
-        response = self.register('test', 'test@example.com', 'FlaskIsAwesome')
-        self.assertEqual(response.status_code, 200)
-        
-        # Attempt to register a user with the same username or email
-        response = self.register('test', 'test2@example.com', 'AnotherPassword')
-        self.assertIn(b'Username already taken.', response.data)
-        
-        response = self.register('anotheruser', 'test@example.com', 'YetAnotherPassword')
-        self.assertIn(b'Email already taken.', response.data)
-    
-    
-    def test_password_mismatch_registration(self):
-        response = self.register('test', 'test@example.com', 'Password123', 'MismatchedPassword')
-        self.assertIn(b'Passwords must match.', response.data)
-
-    def test_weak_password_registration(self):
-        response = self.register('test', 'test@example.com', 'weak', 'weak')
-        self.assertIn(b'Password must be at least 8 characters long and contain', response.data)
-
-    def test_successful_login_after_registration(self):
-        # Register a user
-        self.register('test', 'test@example.com', 'FlaskIsAwesome')
-        
-        # Log in with the registered credentials
-        response = self.app.post('/login', data=dict(
-            username='test',
-            password='FlaskIsAwesome'
-        ), follow_redirects=True)
-        
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Logged in successfully.', response.data)
-    
-    def test_unsuccessful_login_after_registration(self):
-        # Register a user
-        self.register('test', 'test@example.com', 'FlaskIsAwesome')
-        
-        # Attempt to log in with incorrect password
-        response = self.app.post('/login', data=dict(
-            username='test',
-            password='WrongPassword'
-        ), follow_redirects=True)
-        
-        self.assertIn(b'Invalid username or password.', response.data)
-'''
     # checks if a username is betweeen 2 - 20 characters, otherwise, their username is invalid
     def test_invalid_username_registration(self):
         response = self.register('t', 'test@example.com', 'FlaskIsAwesome')
@@ -85,6 +34,22 @@ class UsersTests(unittest.TestCase):
     def test_invalid_email_registration(self):
         response = self.register('test2', 'test@example', 'FlaskIsAwesome')
         self.assertIn(b'Invalid email address.', response.data)
+    
+    def login(self, username, password):
+        return self.app.post('/login',
+                            data=dict(username=username,
+                                      password=password),
+                            follow_redirects=True)
+    
+    # checks if a user successfully logs in
+    def test_successful_login(self):
+        response = self.login(username='test_user', password='test_password')
+        self.assertEqual(response.status_code, 200)  # Assuming successful login redirects to a URL with status 200
+
+    # checks if a user logs in with the incorect password
+    def test_failed_login(self):
+        response = self.login(username='test_user', password='wrong_password')
+        self.assertEqual(response.status_code, 200)  # Assuming failed login redirects to a URL with status 200
 
 
 if __name__ == "__main__":
